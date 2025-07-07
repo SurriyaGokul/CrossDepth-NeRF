bd8435ebaf6777046762b9c11dc93dfc514de279
DineshSK2005 <k.dinesh13412@gmail.com>
2025-07-08 00:02:56 +0530

    Removed

--- render_path.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def render_path(config_yaml, camera_path_json, output_video_path="/kaggle/working/my_render.mp4"):
-    """
-    Render using Nerfstudio CLI and a custom camera path.
-    """
-    os.system(f"ns-render camera-path --load-config {config_yaml} --camera-path-filename {camera_path_json} --output-path {output_video_path}")
-    print("Render completed.")
