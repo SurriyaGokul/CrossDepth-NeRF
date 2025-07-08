ef7bd1213ff5499f86005f7d8b2feb7328534ad8
DineshSiddhartha <143334241+DineshSiddhartha@users.noreply.github.com>
2025-07-08 22:56:48 +0530

    Delete Scene_Training directory

--- Scene_Training/train.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-def run_training(scene_path: str):
-    subprocess.run([
-        "ns-train", "nerfacto",
-        "--data", scene_path,
-        "--viewer.quit-on-train-completion", "True"
-    ], check=True)
